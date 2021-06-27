87ed769078db161c5eb322384653e85f6891e687
Yashaswi627 <64346538+Yashaswi627@users.noreply.github.com>
2021-06-27 19:03:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-h
+song1 = "";
+song2 = "";
+
+function preload() {
+    song1 = loadSound("music.mp3");
+    song2 = loadSound("music2.mp3");
+}
+
+function setup() {
+    canvas = createCanvas(600, 500)
+    canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide;
+}
+
+function draw() {
+    image(video, 0, 0, canvas.width, canvas.height);
+}
